1c3a0962a331de63c4b950e296b1efa60b07c261
madsh <mads.heilberg@gmail.com>
2017-11-06 14:19:53 +0100

    Added function insert logic to support two dimensions

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 var sum =function sum(a, b) {
     return a + b;
 }
+
 var arrays = [0,0,0,0,0,0,0];
 
+for (var i = 0; i < arrays.length; i++) {
+    arrays[i] = [0, 0, 0, 0, 0, 0];
+}
+
 var insert = function insert(a) {
-    for (var i = 0; i < arrays.length; i++) {
-        if (arrays[i] == 0) {
-            arrays[i] = 1;
+
+    for (var i = 0; i < arrays[i].length; i++) {
+        if (arrays[a][i] == 0) {
+            arrays[a][i] = 1;
             break;
         }
     }
--- js/script.test.js
+++ js/script.test.js
@@ -9,9 +9,9 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('inserts into arrray', () => {
 
-    m.insert(1);
-    m.insert(1);
-    m.insert(3);
+    m.insert(0);
+    m.insert(0);
+    m.insert(2);
 
     expect(m.arrays).toEqual([[1,1,0,0,0,0],[0,0,0,0,0,0],[1,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]])
 
