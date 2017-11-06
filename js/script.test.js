6d03143dd24ced3833183b87e0eda4763de94281
flyberson <flyberson12@gmail.com>
2017-11-06 14:06:39 +0100

    removed array spot

--- js/script.test.js
+++ js/script.test.js
@@ -5,15 +5,7 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 
-test('inserts into arrray', () => {
 
-        m.insert(1);
-
-        expect(m.arrays).toEqual([1,0,0,0,0,0,0])
-
-
-
-});
 
 test('inserts into arrray', () => {
 
@@ -21,7 +13,7 @@ test('inserts into arrray', () => {
     m.insert(1);
     m.insert(3);
 
-    expect(m.arrays).toEqual([[1,1,0,0,0,0,0],[0,0,0,0,0,0,0],[1,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]])
+    expect(m.arrays).toEqual([[1,1,0,0,0,0],[0,0,0,0,0,0],[1,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]])
 
 
 
